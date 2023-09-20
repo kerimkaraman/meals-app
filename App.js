262493da1d3d6ab22e5c32ff1ee93e065a6babaa
Kerim Karaman <55785875+kerimkaraman@users.noreply.github.com>
2023-09-20 17:48:39 +0300

    Installing React Navigation and setting some data

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Meals Categories' component={CategoriesScreen} />
+          <Stack.Screen name='Meals Overview' component={MealsOverviewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -18,3 +29,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ */ 
